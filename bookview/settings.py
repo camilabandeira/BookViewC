cd4b808fb0802bd8890d80fc77088f8d34b84958
Camila <yourname@example.com>
2024-10-17 10:08:11 +0100

    Refactor secret key configuration in settings.py

--- bookview/settings.py
+++ bookview/settings.py
@@ -23,9 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_3wzsee7_nu-va559d*g%k386whd)v!cm72*6l0f)q+*n+=vf6'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 SECRET_KEY = config('SECRET_KEY')
