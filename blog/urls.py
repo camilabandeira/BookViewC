584ea38450e33153bfcb006fe1f07f5620a10a18
Camila <yourname@example.com>
2024-10-27 19:51:26 +0000

    Add delete comment functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('reviews/', views.reviews_page, name='reviews'),
     path('about/', views.about_page, name='about'),
     path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+     path('comment/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('profile/update/', views.profile_update, name='profile_update'), 
