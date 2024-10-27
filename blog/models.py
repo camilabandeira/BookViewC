dfbfd7892de8962b7af15fbfb34bab0d6d04dec0
Camila <yourname@example.com>
2024-10-27 18:52:27 +0000

    feat(models): add Profile model with bio, location, and CloudinaryField for profile images

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.templatetags.static import static
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 CATEGORY_CHOICES = (
@@ -40,3 +42,23 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.author.username} on {self.post.title}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    profile_picture = CloudinaryField('image', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}\'s Profile'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
